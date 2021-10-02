ace9a67291681b3ad2516381752c4fc6621d6545
safiq2121 <ilhamrufi13@gmail.com>
2021-10-02 23:52:39 +0700

    first commit

---
+++ proses php.php
@@ -0,0 +1,8 @@
+<?php
+    $nama=$_POST["nama"];
+    $emai=$_POST["email"];
+    $gender=$_POST["gender"];
+    echo "<h1>Halaman proses data</h1>";
+    echo "nama;$nama dan Email;$email";
+    echo "Gender: $gender";
+?>
